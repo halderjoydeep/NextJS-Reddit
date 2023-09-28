c2e8240173526110126fb9320d0e4a41d7ec279d
halderjoydeep <42032921+halderjoydeep@users.noreply.github.com>
2023-09-28 15:29:53 +0530

    Bug fix

--- src/app/r/[slug]/post/[postId]/page.tsx
+++ src/app/r/[slug]/post/[postId]/page.tsx
@@ -15,7 +15,7 @@ interface pageProps {
   params: { postId: string };
 }
 
-export const dynamic = "force-dyanmic";
+export const dynamic = "force-dynamic";
 export const fetchCache = "force-no-store";
 
 const page: React.FC<pageProps> = async ({ params }) => {
